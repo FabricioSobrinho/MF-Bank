d50834f27eae67120599ecc69679f3c84c0636c9
FabricioSobrinho <fabriciomails3@gmail.com>
2023-09-16 14:39:48 -0400

    Feat: created the initial screen

---
+++ src/Pages/InitialScreen.jsx
@@ -0,0 +1,30 @@
+// import images to the page
+import initialScreenImage from "../assets/Images/initial-screen-image.svg";
+
+// import styles to the page
+import styles from "../Styles/InitialScreen.module.css";
+
+// import elements to the page
+import Button from "../Layouts/FormsComponents/Button";
+
+function InitialScreen() {
+  return (
+    <div className={styles.mainInitialPage}>
+      <div className={styles.initialLeftPage}>
+        <div className={styles.text}>
+          <h1>Junte-se a nós, proteja seu patrimônio!</h1>
+          <p>O banco mais seguro da américa latina!</p>
+        </div>
+        <div className={styles.form}>
+          <Button text="Acessar" />
+        </div>
+      </div>
+
+      <div className={styles.initialRightPage}>
+        <img src={initialScreenImage} alt="Savings ico" />
+      </div>
+    </div>
+  );
+}
+
+export default InitialScreen;
