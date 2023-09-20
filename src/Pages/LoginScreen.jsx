d37cd4cd88aaca2b64271aaa98a576bc26d88363
FabricioSobrinho <fabriciomails3@gmail.com>
2023-09-20 15:31:45 -0400

    Fix: iddent of code and remove non use elements

--- src/Pages/LoginScreen.jsx
+++ src/Pages/LoginScreen.jsx
@@ -1,12 +1,34 @@
-import styles from '../Styles/LoginScreen.module.css'
+import styles from "../Styles/LoginScreen.module.css";
 
+import loginScreenImage from "../assets/Images/login-screen-image.svg";
+
+import InputInsertData from "../Layouts/FormsComponents/InputInsertData";
+import InputButton from "../Layouts/FormsComponents/InputButton";
 
 function LoginScreen() {
   return (
-    <div>
-        
-    </div>
-  )
+    <section className={styles.mainLoginPage}>
+      <div className={styles.leftLoginScreen}>
+        <img src={loginScreenImage} />
+      </div>
+      <div className={styles.rightLoginScreen}>
+        <div className={styles.boxForm}>
+          <h1>Entre</h1>
+          <div className={styles.loginForm}>
+            <InputInsertData text="Nome" type="text" name="name" />
+            <InputInsertData text="Senha" type="password" name="pass" />
+            <InputButton text="Entrar" />
+          </div>
+          <div className={styles.createACcountRedirect}>
+            <hr />
+            <p>
+              Ainda não é cliente? <a href="#">Crie uma conta conosco</a> !{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default LoginScreen
+export default LoginScreen;
