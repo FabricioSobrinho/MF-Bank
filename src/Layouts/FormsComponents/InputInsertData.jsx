cfcb7be125a9f4392710e1980bc0ca8e3b3bd193
FabricioSobrinho <fabriciomails3@gmail.com>
2023-09-20 15:25:51 -0400

    Fix: removed labels in the inputs model

--- src/Layouts/FormsComponents/InputInsertData.jsx
+++ src/Layouts/FormsComponents/InputInsertData.jsx
@@ -1,10 +1,17 @@
-import styles from '../../Styles/InputInsertData.module.css'
+import styles from "../../Styles/InputInsertData.module.css";
 
 // eslint-disable-next-line react/prop-types
-function InputInsertData({type, text}) {
+function InputInsertData({ type, text, name }) {
   return (
-    <input type={type} value={text} className={styles.input}/>
-  )
+    <>
+      <input
+        type={type}
+        className={styles.input}
+        placeholder={text}
+        name={name}
+      />
+    </>
+  );
 }
 
-export default InputInsertData
+export default InputInsertData;
