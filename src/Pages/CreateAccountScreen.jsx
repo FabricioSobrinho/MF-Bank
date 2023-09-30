c26cb9296335101b1398475cf395c285b7ad6454
FabricioSobrinho <fabriciomails3@gmail.com>
2023-09-30 10:21:14 -0400

    Feat: Creation of the account creation screen and styling for the page.

--- src/Pages/CreateAccountScreen.jsx
+++ src/Pages/CreateAccountScreen.jsx
@@ -1,8 +1,57 @@
+import styles from "../Styles/CreateAccountScreen.module.css";
+
+import createAccountImage from "../assets/Images/create-account-screen-image.svg";
+
+import InputInsertData from "../Layouts/FormsComponents/InputInsertData";
+import InputButton from "../Layouts/FormsComponents/InputButton";
 
 function CreateAccountScreen() {
   return (
-    <div>CreateAccountScreen</div>
-  )
+    <div className={styles.mainCreateAccountPage}>
+      <div className={styles.leftCreateAccountScreen}>
+        <img src={createAccountImage} />
+      </div>
+      <div className={styles.rightCreateAccountScreen}>
+        <div className={styles.rightCreateAccountScreenContainer}>
+          <div className={styles.form}>
+            <h1>INSIRA SEUS DADOS</h1>
+            <InputInsertData heightInput={4.2} widthInput={22.5} text="Nome" />
+            <InputInsertData
+              heightInput={4.2}
+              widthInput={22.5}
+              text="Sobrenome"
+            />
+            <InputInsertData heightInput={4.2} widthInput={22.5} text="Email" />
+            <InputInsertData
+              heightInput={4.2}
+              widthInput={22.5}
+              text="Telefone"
+            />
+            <InputInsertData heightInput={4.2} widthInput={22.5} text="UF" />
+            <InputInsertData heightInput={4.2} widthInput={22.5} text="CEP" />
+            <InputInsertData heightInput={4.2} widthInput={22.5} text="CPF" />
+            <InputInsertData
+              heightInput={4.2}
+              widthInput={22.5}
+              text="Cidade"
+            />
+            <InputInsertData heightInput={4.2} widthInput={22.5} text="Senha" />
+            <InputInsertData
+              heightInput={4.2}
+              widthInput={22.5}
+              text="Confirmar senha"
+            />
+
+            <InputButton
+              heightButton={5.5}
+              widthButton={23}
+              text={"CRIAR CONTA"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CreateAccountScreen
+export default CreateAccountScreen;
