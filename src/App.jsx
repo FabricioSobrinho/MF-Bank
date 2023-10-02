383fadb534461b0b19229988492e24e7d783e116
FabricioSobrinho <fabriciomails3@gmail.com>
2023-10-02 14:21:40 -0400

    Fix: fixed name of components; Feat: added withdraw component and route

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "./Layouts/Components/Container";
 
 // Import pages
-import InitialScreen from "./Pages/InitialPage";
-import LoginScreen from "./Pages/LoginPage";
-import CreateAccountScreen from "./Pages/CreateAccountPage";
+import InitialPage from "./Pages/InitialPage";
+import LoginPage from "./Pages/LoginPage";
+import CreateAccountPage from "./Pages/CreateAccountPage";
 import ViewAccountPage from "./Pages/ViewAccountPage";
+import WithdrawPage from "./Pages/WithdrawPage";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  setIsLoggedIn(true);
   return (
     <>
       <Router>
         {isLoggedIn ? <LoggedNavBar />:<NavBar /> } 
         <Container>
           <Routes>
-            <Route exact element={<InitialScreen />} path="/" />
-            <Route exact element={<LoginScreen />} path="/login" />
-            <Route exact element={<CreateAccountScreen />} path="/create-acc" />
+            <Route exact element={<InitialPage />} path="/" />
+            <Route exact element={<LoginPage />} path="/login" />
+            <Route exact element={<CreateAccountPage />} path="/create-acc" />
             <Route exact element={<ViewAccountPage />} path="/view-account" />
+            <Route exact element={<WithdrawPage />} path="/withdraw" />
           </Routes>
         </Container>
       </Router>
