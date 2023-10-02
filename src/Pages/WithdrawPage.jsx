597508f14223829c2c1df6ebdf9137914db670aa
FabricioSobrinho <fabriciomails3@gmail.com>
2023-10-02 15:04:29 -0400

    Feat: The withdrawal page and its styles implemented

--- src/Pages/WithdrawPage.jsx
+++ src/Pages/WithdrawPage.jsx
@@ -1,8 +1,50 @@
+// import styles
+import styles from "../Styles/WithdrawPage.module.css";
+
+// import components 
+import InputButton from "../Layouts/FormsComponents/InputButton";
+import InputInsertData from "../Layouts/FormsComponents/InputInsertData";
 
 function WithdrawPage() {
   return (
-    <div>WithdrawPage</div>
-  )
+    <div className={styles.MainWithdrawPage}>
+      <div className={styles.leftWithdrawScreen}>
+        <div className={styles.topLeftWithdrawScreen}>
+          <label htmlFor="Quantia do saque">
+            Insira a quantia que deseja sacar:{" "}
+          </label>
+          <InputInsertData
+            text="Quantia do saque"
+            heightInput={4.5}
+            widthInput={29}
+          />
+          <InputButton text="Confirmar" heightButton={5} widthButton={15} />
+          <div className={styles.EnoughBalance}>Saldo suficiente</div>
+          <div className={styles.notEnoughBalance}>Saldo insuficiente</div>
+          <hr />
+        </div>
+        <div className={styles.bottomLeftWithdrawScreen}>
+          <label htmlFor="Senha">Insira a sua senha: </label>
+          <InputInsertData text="Senha" heightInput={4.5} widthInput={29} />
+          <InputButton text="Confirmar" heightButton={5} widthButton={15} />
+        </div>
+      </div>
+      <div className={styles.rightWithdrawScreen}>
+        <div className={styles.newBalanceView}>
+          <p>
+            Saldo atual: x
+            <br />
+            Novo saldo: x - withdrawMontant
+          </p>
+        </div>
+        <InputButton
+          text="Cancelar operação"
+          heightButton={5.125}
+          widthButton={29}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default WithdrawPage
+export default WithdrawPage;
