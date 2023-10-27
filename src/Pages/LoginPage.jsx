32bc930508ae34007e16395b57425d488073dcf2
FabricioSobrinho <fabriciomails3@gmail.com>
2023-10-27 10:23:46 -0400

    Chore: started login implement

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,8 +4,40 @@ import loginScreenImage from "../assets/Images/login-screen-image.svg";
 
 import InputInsertData from "../Layouts/FormsComponents/InputInsertData";
 import InputButton from "../Layouts/FormsComponents/InputButton";
+import axios from "axios";
+import { useState } from "react";
 
 function LoginScreen() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLoginData = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+    console.log(loginData)
+  };
+
+  const loginUser = async () => {
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/auth/sign_in",
+        loginData,
+        config
+      );
+
+      console.log("Usuário logado com sucesso:", response);
+    } catch (error) {
+      console.log("Houve um erro na autenticação" + error);
+    }
+  };
+
   return (
     <section className={styles.mainLoginPage}>
       <div className={styles.leftLoginScreen}>
@@ -16,22 +48,29 @@ function LoginScreen() {
           <h1>Entre</h1>
           <div className={styles.loginForm}>
             <InputInsertData
-              text="Nome"
+              text="Email"
               type="text"
-              name="name"
+              name="email"
               widthInput={32}
               heightInput={5}
               required
-            />
+              handleChange={handleLoginData}
+              />
             <InputInsertData
               text="Senha"
               type="password"
-              name="pass"
+              name="password"
               widthInput={32}
               heightInput={5}
               required
+              handleChange={handleLoginData}
             />
-            <InputButton text="Entrar" heightButton={4.5} widthButton={20} />
+            <InputButton
+              text="Entrar"
+              heightButton={4.5}
+              widthButton={20}
+              handleClick={loginUser}
+            />
           </div>
           <div className={styles.createACcountRedirect}>
             <hr />
