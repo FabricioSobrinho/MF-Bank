0b7c2fca1ba58fb67b3b241da10472aaa0902db8
FabricioSobrinho <fabriciomails3@gmail.com>
2023-11-06 14:48:16 -0400

    refactor: updated the errors component

--- src/Layouts/Components/Errors.jsx
+++ src/Layouts/Components/Errors.jsx
@@ -1,16 +1,17 @@
 import styles from "../../Styles/Errors.module.css";
+import Error from "./Error";
 
 function Errors({ errors }) {
   return (
     <div className={styles.errors}>
       {errors.length > 0 &&
-        errors.map((item) => {
-          if (Array.isArray(item)) {
-            return item.map((err) => <p key={err}>{err}</p>);
-          } else {
-            return <p key={item}>{item}</p>;
-          }
-        })}
+        errors.map((error) => (
+          <p key={error}>
+            {error.map((itemError) => (
+              <Error text={itemError} key={itemError}/>
+            ))}
+          </p>
+        ))}
     </div>
   );
 }
