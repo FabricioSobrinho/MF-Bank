307798eee6e136ae03e3149c51e2ba8bf04efec2
FabricioSobrinho <fabriciomails3@gmail.com>
2023-11-06 10:27:14 -0400

    Feat: created the showing current user logic

--- src/Pages/ViewAccountPage.jsx
+++ src/Pages/ViewAccountPage.jsx
@@ -1,15 +1,48 @@
-import InputButton from "../Layouts/FormsComponents/InputButton";
+// import hooks
+import { useEffect, useState } from "react";
+import { useUser } from "../hooks/UserProvider";
+import { validateToken } from "../hooks/ValidateToken";
+
+// import styles
 import styles from "../Styles/ViewAccountPage.module.css";
 
+// import components
+import InputButton from "../Layouts/FormsComponents/InputButton";
+
+// import dependencies
+import Cookies from "js-cookie";
+
 function ViewAccountPage() {
+  const { user, setUserData } = useUser();
+
+  const accessToken = Cookies.get("accessToken");
+  const client = Cookies.get("client");
+  const uid = Cookies.get("uid");
+
+  const validateTokenAuth = async () => {
+    try {
+      await validateToken(accessToken, client, uid, setUserData);
+    } catch (error) {
+      console.log("Houve um erro de validação: " + error);
+    }
+  };
+
+  // validateToken
+  if (accessToken && client) {
+    useEffect(() => {
+      validateTokenAuth();
+    }, []);
+  } else {
+  }
+
   return (
-    <div className={styles.mainViewAccountPage}>
+    <div className={styles.mainViewAccountPage} onClick={validateToken}>
       <div className={styles.leftViewAccountPage}>
         <div className={styles.accInfo}>
           <p>Dados da conta:</p>
-          <p>Nome Titular: </p>
+          <p>Nome Titular: {user.name}</p>
           <p>Saldo: </p>
-          <p>Número da conta:</p>
+          <p>Número da conta: {user.acc_number}</p>
         </div>
         <div className={styles.accLastActions}>
           <p>Últimas movimentações: </p>
@@ -24,23 +57,41 @@ function ViewAccountPage() {
             <h3>MENU</h3>
           </div>
           <div className={styles.menuButtons}>
-            <InputButton text="Saque" heightButton={5} widthButton={20} />
-            <InputButton text="Deposito" heightButton={5} widthButton={20} />
+            <InputButton
+              text="Saque"
+              heightButton={5}
+              widthButton={20}
+              route={"/withdraw"}
+            />
+            <InputButton
+              text="Deposito"
+              heightButton={5}
+              widthButton={20}
+              route={"deposit"}
+            />
             <InputButton
               text="Transferência"
               heightButton={5}
               widthButton={20}
+              route={"/transfer"}
             />
-            <InputButton text="Extrato" heightButton={5} widthButton={20} />
             <InputButton
-              text="Editar informções"
+              text="Extrato"
               heightButton={5}
               widthButton={20}
+              route={"/extract"}
+            />
+            <InputButton
+              text="Editar informações"
+              heightButton={5}
+              widthButton={20}
+              route={"/edit"}
             />
             <InputButton
               text="Fechar conta"
               heightButton={5}
               widthButton={20}
+              route={"/close-account"}
             />
           </div>
         </menu>
