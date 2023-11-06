17821c304a93966e1b9dbf182c8e303fd6221d0e
FabricioSobrinho <fabriciomails3@gmail.com>
2023-11-06 10:19:22 -0400

    Chore: added the cookies for the memory of application

--- src/Layouts/Components/CookiesAllow.jsx
+++ src/Layouts/Components/CookiesAllow.jsx
@@ -1,30 +1,46 @@
+// import hooks
 import { useRef } from "react";
+
+// import styles
 import styles from "../../Styles/CookiesAllow.module.css";
+
+// import components
 import InputButton from "../FormsComponents/InputButton";
+
+//import dependencies
+import Cookies from "js-cookie";
 
 function CookiesAllow() {
   const cookiesMessage = useRef(null);
+  let showCookiesMessage;
 
   const cookiesAllowed = () => {
     cookiesMessage.current.style.display = "none";
+    showCookiesMessage = Cookies.set("showCookiesDisable", "true", {
+      expires: 7,
+    });
   };
 
   return (
-    <div className={styles.cookieAllow} ref={cookiesMessage}>
-      <div className={styles.mainCookiesAllow}>
-        <p>
-          Nossa plataforma utiliza cookies para garantir que você tenha a melhor
-          experiência.
-        </p>
+    <>
+      {!Cookies.get("showCookiesDisable") && (
+        <div className={styles.cookieAllow} ref={cookiesMessage}>
+          <div className={styles.mainCookiesAllow}>
+            <p>
+              Nossa plataforma utiliza cookies para garantir que você tenha a
+              melhor experiência.
+            </p>
 
-        <InputButton
-          text="Concordo"
-          heightButton={5}
-          widthButton={15}
-          handleClick={cookiesAllowed}
-        />
-      </div>
-    </div>
+            <InputButton
+              text="Concordo"
+              heightButton={5}
+              widthButton={15}
+              handleClick={cookiesAllowed}
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
