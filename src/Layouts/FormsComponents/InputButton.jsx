eb2acf5d206c78f1fd6ab9b5f7052037688e1ce8
FabricioSobrinho <fabriciomails3@gmail.com>
2023-11-06 10:22:37 -0400

    Chore: removed unused code

--- src/Layouts/FormsComponents/InputButton.jsx
+++ src/Layouts/FormsComponents/InputButton.jsx
@@ -1,29 +1,35 @@
-import styled from "styled-components";
-
 // eslint-disable-next-line react/prop-types
-function InputButton({ text, widthButton, heightButton, handleClick }) {
-  const InputButton = styled.button`
-    height: ${heightButton}rem;
-    width: ${widthButton}rem;
-    margin: 0.8rem;
-    background: #2d3136;
-    color: #f0f8ff;
-    border-radius: 0.3125rem;
-    border: none;
-    outline: none;
-    box-shadow: 0px 0px 40px -15px #000000;
-    font-size: 1.7rem;
-    text-transform: uppercase;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-    transition: 0.4s;
-    &:hover {
-      background: #44484d;
-      cursor: pointer;
-    }
-  `;
-  return <InputButton onClick={handleClick}>{text}</InputButton>;
+function InputButton({
+  text,
+  widthButton,
+  heightButton,
+  handleClick,
+  route,
+}) {
+  const styles = {
+    height: `${heightButton}rem`,
+    width: `${widthButton}rem`,
+    margin: `0.8rem`,
+    background: ` #2d3136`,
+    color: `#f0f8ff`,
+    borderRadius: `0.3125rem`,
+    border: `none`,
+    outline: `none`,
+    boxShadow: `0px 0px 40px -15px #000000`,
+    fontSize: ` 1.7rem`,
+    textTransform: `uppercase`,
+    fontStyle: `normal`,
+    fontWeight: `700`,
+    lineHeight: `normal`,
+    transition: `0.4s`,
+
+    cursor: "pointer",
+  };
+  return (
+    <a href={route}>
+      <input type="button" style={styles} onClick={handleClick} value={text} />
+    </a>
+  );
 }
 
 export default InputButton;
