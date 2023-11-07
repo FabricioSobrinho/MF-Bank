f6e725fa1ae409eba79eb29b7d075e0c0b97bf88
FabricioSobrinho <fabriciomails3@gmail.com>
2023-11-07 16:01:16 -0400

    Feat: started implement of the api in the withdraw page

--- src/Pages/WithdrawPage.jsx
+++ src/Pages/WithdrawPage.jsx
@@ -1,3 +1,9 @@
+// import hooks
+import { useUser } from "../hooks/UserProvider";
+import { validateToken } from "../hooks/ValidateToken";
+import { GetBalance } from "../hooks/GetBalance";
+import { ValidatePassword } from "../hooks/ValidatePassword";
+
 // import styles
 import styles from "../Styles/WithdrawPage.module.css";
 
@@ -5,14 +11,96 @@ import styles from "../Styles/WithdrawPage.module.css";
 import InputButton from "../Layouts/FormsComponents/InputButton";
 import InputInsertData from "../Layouts/FormsComponents/InputInsertData";
 
+// import dependencies
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function WithdrawPage() {
+  const { user, setUserData, setBalance } = useUser();
+
+  const [withdrawMontant, setWIthdrawMontant] = useState({
+    withdrawal_amount: 0
+  });
+
+  const [password, setPassword] = useState();
+
+  const navigate = useNavigate();
+
+  const accessToken = Cookies.get("accessToken");
+  const client = Cookies.get("client");
+  const uid = Cookies.get("uid");
+
+  const withdrawAction = async () => {
+    try {
+      const isValidPass = await ValidatePassword(
+        accessToken,
+        client,
+        uid,
+        password
+      );
+
+      if (isValidPass) {
+        try {
+          const config = {
+            headers: {
+              "access-token": accessToken,
+              client: client,
+              uid: uid,
+            },
+          };
+          const response = await axios.post(
+            "http://localhost:3000/withdraw",
+            withdrawMontant,
+            config
+          );
+
+          navigate("/view-account")
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        console.log("Invalid pass");
+      }
+    } catch (error) {
+      console.log("Houve um erro na validação da sua senha");
+    }
+  };
+
+  const handleWithdraw = (e) => {
+    const convertedMontant = Number(e.target.value);
+
+    setWIthdrawMontant({withdrawal_amount: convertedMontant});
+  };
+
+  const handlePassword = async (e) => {
+    setPassword(e.target.value);
+  };
+
+  const validateTokenAuth = async () => {
+    try {
+      await validateToken(accessToken, client, uid, setUserData);
+      if (accessToken) {
+        await GetBalance(accessToken, client, uid, setBalance);
+      }
+    } catch (error) {
+      console.log("Houve um erro de validação: " + error);
+    }
+  };
+
+  useEffect(() => {
+    if (accessToken) {
+      validateTokenAuth();
+    }
+  }, []);
 
   return (
     <div className={styles.MainWithdrawPage}>
       <div className={styles.leftWithdrawScreen}>
         <div className={styles.topLeftWithdrawScreen}>
           <label htmlFor="withdrawMontant">
-            Insira a quantia que deseja sacar:{" "}
+            Insira a quantia que deseja sacar:
           </label>
           <InputInsertData
             text="Quantia do saque"
@@ -20,11 +108,14 @@ function WithdrawPage() {
             widthInput={29}
             type="number"
             name="withdrawMontant"
-            mask="999.999.999.999.99"
+            handleChange={handleWithdraw}
             required
           />
-          <div className={styles.EnoughBalance}>Saldo suficiente</div>
-          <div className={styles.notEnoughBalance}>Saldo insuficiente</div>
+          {user.balance > withdrawMontant ? (
+            <div className={styles.EnoughBalance}>Saldo suficiente</div>
+          ) : (
+            <div className={styles.notEnoughBalance}>Saldo insuficiente</div>
+          )}
           <hr />
         </div>
         <div className={styles.bottomLeftWithdrawScreen}>
@@ -35,6 +126,7 @@ function WithdrawPage() {
             widthInput={29}
             type="password"
             name="withdrawPass"
+            handleChange={handlePassword}
             required
           />
           <InputButton
@@ -42,15 +134,16 @@ function WithdrawPage() {
             heightButton={5}
             widthButton={15}
             name="cofirmWithdraw"
+            handleClick={withdrawAction}
           />
         </div>
       </div>
       <div className={styles.rightWithdrawScreen}>
         <div className={styles.newBalanceView}>
           <p>
-            Saldo atual: x
+            Saldo atual: {user.balance}
             <br />
-            Novo saldo: x - withdrawMontant
+            Novo saldo: {user.balance - withdrawMontant.withdrawal_amount}
           </p>
         </div>
         <InputButton
